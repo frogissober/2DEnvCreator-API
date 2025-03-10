54a4be3ec09d2b8a9eb156091725756fc8996ccd
Floris Bogers <71845444+frogissober@users.noreply.github.com>
2025-03-10 14:10:29 +0100

    addded token based auth

--- 2DEnvCreator-API/Program.cs
+++ 2DEnvCreator-API/Program.cs
@@ -1,7 +1,28 @@
+﻿using System.Data.Common;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var sqlConnectionString = builder.Configuration["SqlConnectionString"];
+var sqlConnectionStringFound = !string.IsNullOrEmpty(sqlConnectionString);
 
+// Adding the HTTP Context accessor to be injected. This is needed by the AspNetIdentityUserRepository
+// to resolve the current user.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<IAuthenticationService, AspNetIdentityAuthenticationService>();
+
+// 
+builder.Services.AddAuthorization();
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>()
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = sqlConnectionString;
+    });
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -14,9 +35,15 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapGet("/", () => $"The API is up . Connection string found: {(sqlConnectionStringFound ? "✅" : "❌")}");
+
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
+
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
 
